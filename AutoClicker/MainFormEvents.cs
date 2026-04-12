63d94fb95165ba6eeadd634aa87ed56e2a4d9550
pettro98 <ya.skywalker@yandex.ru>
2026-04-12 14:29:56 +0300

    WIP start implementing mvvm

---
+++ AutoClicker/MainFormEvents.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClicker
+{
+    internal class MainFormEvents
+    {
+        public enum PositioningType
+        {
+            Cursor = 0,
+            ScreenRelative,
+            WindowRelative
+        }
+
+        public delegate void IntervalChangedHandler(uint newInterval);
+        public event IntervalChangedHandler? IntervalChangedMin;
+        public event IntervalChangedHandler? IntervalChangedSec;
+        public event IntervalChangedHandler? IntervalChangedMsec;
+        public event IntervalChangedHandler? IntervalChangedOffsetMsec;
+
+        public delegate void PositioningTypeChangedHandler(PositioningType newPositioningType);
+        public event PositioningTypeChangedHandler? PositioningTypeChanged;
+
+        public delegate void PositionChangedHandler(uint newCoordinate);
+        public event PositionChangedHandler? PositionXChanged;
+        public event PositionChangedHandler? PositionYChanged;
+
+        public delegate void IntervalChangedHandler(uint intervalMsec, uint offsetMsec);
+        public event IntervalChangedHandler? IntervalChanged;
+
+        public delegate void IntervalChangedHandler(uint intervalMsec, uint offsetMsec);
+        public event IntervalChangedHandler? IntervalChanged;
+
+        public delegate void IntervalChangedHandler(uint intervalMsec, uint offsetMsec);
+        public event IntervalChangedHandler? IntervalChanged;
+
+
+    }
+}
